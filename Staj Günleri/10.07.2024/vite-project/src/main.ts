ff6d2b3c98510c8dff27c91c027329c729ef96bb
songulgurel <gureelsongul@gmail.com>
2024-07-18 17:24:11 +0300

    08.07.2024 staj günü

--- Staj Günleri/10.07.2024/vite-project/src/main.ts
+++ Staj Günleri/10.07.2024/vite-project/src/main.ts
@@ -2,4 +2,11 @@ import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
 
+import ElementPlus from 'element-plus'
+import 'element-plus/lib/theme-chalk/index.css'
+
+createApp(App)
+  .use(ElementPlus)
+  .mount('#app')
+
 createApp(App).mount('#app')
